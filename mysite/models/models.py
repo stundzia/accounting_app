aae8985bd59ede37b6e83819b9b20b55b1f58a62
stundzia <pst@boolit.eu>
2016-10-10 21:03:22 +0300

    [ADD] webapp: init
    Index page.

--- mysite/models/models.py
+++ mysite/models/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=70)
-    vat_payer = models.BooleanField() # Check syntax
+    vat_payer = models.BooleanField()  # Check syntax
     vat_number = models.CharField(max_length=20)
     reg_number = models.CharField(max_length=20)
 
