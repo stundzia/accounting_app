a9ede0cf23f21bda92b7adae33d8ecb84320b8e5
stundzia <pst@boolit.eu>
2016-11-14 21:43:57 +0200

    [IMP] webapp: home page and views

--- webapp/views.py
+++ webapp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Welcome!</h1>")
+    # return HttpResponse("<h1>Welcome!</h1>")
+    return render(request, 'home.html')
